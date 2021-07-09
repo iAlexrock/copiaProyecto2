2849d1e3dec3e6f3e66aa9e6a3f122b0547061f3
Eldrick Mnedoza <eldrickeldrickeldrick@gmail.com>
2021-07-09 13:51:26 -0500

    ooopp

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const logeo = require('./routes/log');
 const logout = require('./routes/Logout');
 const admin = require('./routes/Admin'); //ruta para entrar como admin
 const lider = require('./routes/Lider'); //rutas para entrar como lider
-
+const home = require('./routes/home');
 
 ///app.use('/sign-up' , logeados); //vanessa
 app.use('/sign-in' , ingreso); //eldrick
@@ -89,6 +89,8 @@ app.use('/log' , logeo); //eldrick
 app.use('/logout' , logout); //eldrick
 app.use('/admin',admin); //alejo <3 
 app.use('/lider',lider); //rodrigo
+
+app.use('/home' , home); //eldrick
 
 app.use('/organizador' , organizador); 
 app.use('/espectador' , espectador); 
